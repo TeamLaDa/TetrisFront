db8543fbdf6918c94280ca1851c349fee34ec365
Damien Lamalle <damien.lamalle@soprasteria.com>
2017-06-13 14:22:53 +0200

    Permet l'affichage des meilleurs scores en front

---
+++ WebContent/app/components/score/controller.js
@@ -0,0 +1,26 @@
+var app = angular.module("TetrisFront");
+
+
+/*
+ * Crée le controller pour la page score
+ */
+app.controller("scoreController", function($scope, Page, $routeParams, ScoreRessource) {
+	Page.setTitle("Score");
+
+  $scope.scores = ScoreRessource.query();
+
+
+
+
+});
+
+
+
+/*
+ * Crée un factory permettant de récupérer l'URL
+ */
+app.factory('ScoreRessource', ['$resource', 'API_URL', function($resource, API_URL) {
+  return $resource(API_URL + 'score', null, {
+    'add': { method: 'PUT'}
+  });
+}]);
--- WebContent/app/routes.js
+++ WebContent/app/routes.js
@@ -1,4 +1,4 @@
-var app = angular.module("TetrisFront"); 
+var app = angular.module("TetrisFront");
 
 
 app.config(function($routeProvider) {
@@ -6,5 +6,13 @@ app.config(function($routeProvider) {
 		.when("/", {
 			templateUrl: "./app/components/home/home.html",
 			controller: "homeController"
+		})
+		.when("/tetris", {
+			templateUrl: "./app/components/tetris/tetris.html",
+			controller: "tetrisController"
+		})
+		.when("/score", {
+			templateUrl: "./app/components/score/score.html",
+			controller: "scoreController"
 		});
-}); 
+});
